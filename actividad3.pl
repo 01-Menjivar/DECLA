257486dd4077f02389b0ab1384637417d446738d
FabioMijango <00156022@uca.edu.sv>
2025-08-25 16:41:47 -0600

    Actividad 3

---
+++ actividad3.pl
@@ -0,0 +1,52 @@
+matiene(aldeas,lazos familiares)
+matiene(aldeas,lazos comerciales)
+matiene(aldeas,lazos politicos)
+
+vive(Rivoria, Don Esteban)
+vive(Riboria, Doña Mariela)
+
+hijo(Clara, Don Esteban)
+hijo(Clara, Doña Mariela)
+
+hijo(Rodolfo, Don Esteban)
+hijo(Rodolfo, Doña Mariela)
+
+hijo(Ismael, Don Esteban)
+hijo(Ismael, Doña Mariela)
+
+matrimonio(Clara, Andres)
+
+hijo(Sofía, Clara)
+hijo(Sofía, Andres)
+
+matrimonio(Rodolfo, Teresa)
+
+hijo(Lucas, Rodolfo)
+hijo(Lucas, Martina)
+
+hijo(Martina, Rodolfo)
+hijo(Martina, Martina)
+
+amigo(Ismael, Hector)
+
+vive(Montelar, Don Alonso)
+vive(Montelar, Doña Beatriz)
+
+hijo(Felipe, Don Alonso)
+hijo(Felipe, Doña Beatriz)
+
+
+hijo(Catalina, Don Alonso)
+hijo(Catalina, Doña Beatriz)
+
+
+hijo(Emilia, Don Alonso)
+hijo(Emilia, Doña Beatriz)
+
+explora(Felipe, montañas)
+explora(Felipe, bosques)
+
+conocimientos(Catalina, hierbas medicinales)
+conocimientos(Emilia, ceramica)
+
+matrimonio(Catalina, Andrés)
